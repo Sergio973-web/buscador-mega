3c5a566b79ad63e94c1de63ee5bbc7498c4c3f68
Sergio Guerra <sergitario786@gmail.com>
2025-11-28 06:13:11 -0300

    Agregar búsqueda por imagen

---
+++ api/compare.js
@@ -0,0 +1,17 @@
+import { buscarImagenSimilar } from '../utils/compareImages';
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Método no permitido' });
+  }
+
+  const { url } = req.body;
+
+  if (!url) {
+    return res.status(400).json({ error: 'Falta URL de imagen' });
+  }
+
+  const resultados = await buscarImagenSimilar(url);
+
+  res.status(200).json({ resultados });
+}
