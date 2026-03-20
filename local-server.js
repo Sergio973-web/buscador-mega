ff116a177cd46dae0a20cfc3dad4074f453e9f44
Your Name <you@example.com>
2026-03-20 07:20:04 -0300

    Actualización

--- local-server.js
+++ local-server.js
@@ -287,7 +287,7 @@ app.get("/api/reload", (req, res) => {
 // ===============================
 // START SERVER
 // ===============================
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 Servidor activo");
   console.log(`📡 Puerto: ${PORT}`);
 });
